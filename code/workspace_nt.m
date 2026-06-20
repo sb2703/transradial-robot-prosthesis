cfc8316abe70407b51b1f815c927c1d62ebf8eae
Samuele Bani <110915332+sb2703@users.noreply.github.com>
2026-06-20 17:06:55 +0200

    Add files via upload

--- code/workspace_nt.m
+++ code/workspace_nt.m
@@ -1,9 +1,16 @@
-function ps = workspace_nt(roms, pars)
+function ps = workspace_nt(roms, a)
 % Computes workspace of given non-thumb finger.
-% IN - roms: Ranges of Motion (RoMs) of finger
-% IN - pars: anthropometric parameters of finger
+% IN - roms: Ranges of Motion (RoMs) of non-thumb finger
+% IN - pars: anthropometric parameters of non-thumb finger
 % OUT - ps: points representing workspace
 
+% Extraction of anthropometric parameters of non-thumb finger
+a_nt1 = a(1);
+a_nt2 = a(2);
+a_nt3 = a(3);
+a_nt4 = a(4);
+
+% Computation of workspace of non-thumb finger
 ps = [];
 for i = roms(1, :)
   for j = roms(2, :)
@@ -14,9 +21,9 @@ for i = roms(1, :)
       s_2 = sin(j);
       s_3 = sin(j + k);
       s_4 = sin(j + 5*k/3);
-      new_point = [cos(i).*(pars(1) + pars(2)*c_2 + pars(3)*c_3 + pars(4)*c_4);
-                   sin(i).*(pars(1) + pars(2)*c_2 + pars(3)*c_3 + pars(4)*c_4);
-                   pars(2)*s_2 + pars(3)*s_3 + pars(4)*s_4];
+      new_point = [cos(i).*(a_nt1 + a_nt2*c_2 + a_nt3*c_3 + a_nt4*c_4);
+                   sin(i).*(a_nt1 + a_nt2*c_2 + a_nt3*c_3 + a_nt4*c_4);
+                   a_nt2*s_2 + a_nt3*s_3 + a_nt4*s_4];
       ps = [ps, new_point];
     end
   end
--- code/workspace_t.m
+++ code/workspace_t.m
@@ -1,10 +1,17 @@
-function ps = workspace_t(roms, pars, R)
-% Computes workspace of thumb finger.
+function ps = workspace_t(roms, a, R)
+% Computes workspace of thumb.
 % IN - roms: Ranges of Motion (RoMs) of thumb
 % IN - pars: anthropometric parameters of thumb
 % IN - R: rotation matrix from wrist to thumb base
 % OUT - ps: points representing workspace
 
+% Extraction of anthropometric parameters of thumb
+a_t1 = a(1);
+a_t2 = a(2);
+a_t3 = a(3);
+a_t4 = a(4);
+
+% Computation of workspace of non-thumb finger
 ps = [];
 for i = roms(1, :)
   for j = roms(2, :)
@@ -16,9 +23,9 @@ for i = roms(1, :)
         s_2 = sin(j);
         s_3 = sin(j + k);
         s_4 = sin(j + k + l);
-        new_point = [cos(i).*(pars(1) + pars(2)*c_2 + pars(3)*c_3 + pars(4)*c_4);
-                     sin(i).*(pars(1) + pars(2)*c_2 + pars(3)*c_3 + pars(4)*c_4);
-                     pars(2)*s_2 + pars(3)*s_3 + pars(4)*s_4];
+        new_point = [cos(i).*(a_t1 + a_t2*c_2 + a_t3*c_3 + a_t4*c_4);
+                     sin(i).*(a_t1 + a_t2*c_2 + a_t3*c_3 + a_t4*c_4);
+                     a_t2*s_2 + a_t3*s_3 + a_t4*s_4];
         new_point = R*new_point;
         ps = [ps, new_point];
       end
